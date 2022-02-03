6455f63f5d8e44997f97b776d0caf39fb8bc9be9
VyargizovaEG <fakel666@bk.ru>
2022-02-03 15:41:02 +0400

    030222

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/BrowserEx.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/BrowserEx.java
@@ -1,70 +1,107 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.compress.harmony.unpack200.bytecode.forms.NoArgumentForm;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserEx {
     private WebDriver driver;
 
     @BeforeClass
 
-    public void setDriver() {
+    public void setup() {
         String br = BrowserType.CHROME;
-        if (br == BrowserType.CHROME) {
+        if (br.equals(BrowserType.CHROME)) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-        } else if (br == BrowserType.FIREFOX) {
+        } else if (br.equals(BrowserType.FIREFOX)) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
-        } else if (br == BrowserType.OPERA); {
+        } else if (br.equals(BrowserType.OPERA)) ;
+        {
             WebDriverManager.operadriver().setup();
             driver = new OperaDriver();
         }
 
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
     }
-
 
     @Test
     public void testUntitledTestCase() {
-        driver.get("http://localhost/addressbook/");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        gotoPage();
+        Login("admin", "secret");
+        fillForm();
+
+    }
+
+    private void type(By.ByXPath locator, String text) {
+        if (text != null) {
+            String existingTest = driver.findElement(locator).getAttribute("value");
+            if (!text.equals(existingTest)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    public void fillForm() {
         driver.findElement(By.linkText("add new")).click();
         driver.findElement(By.name("firstname")).click();
         driver.findElement(By.name("firstname")).clear();
-        driver.findElement(By.name("firstname")).sendKeys("rty");
+        driver.findElement(By.name("firstname")).sendKeys("test1");
         driver.findElement(By.name("lastname")).click();
         driver.findElement(By.name("lastname")).clear();
-        driver.findElement(By.name("lastname")).sendKeys("ghjiijjj");
+        driver.findElement(By.name("lastname")).sendKeys("test2");
         driver.findElement(By.name("mobile")).click();
         driver.findElement(By.name("mobile")).clear();
-        driver.findElement(By.name("mobile")).sendKeys("99993652235");
+        driver.findElement(By.name("mobile")).sendKeys("test3");
         driver.findElement(By.name("theform")).click();
         driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
         driver.findElement(By.linkText("home")).click();
-    }
-
-    @AfterClass
-    private void tearDown() {
-        driver.quit();
 
     }
+
+    private void Login(String log, String pass) {
+        driver.findElement(By.name("user")).sendKeys(log);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(pass);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    private void gotoPage() {
+        driver.get("http://localhost/addressbook/");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+        @AfterClass
+        public void tearDown () {
+            driver.quit();
+        }
+
 }
+
+
+
+
 
 
